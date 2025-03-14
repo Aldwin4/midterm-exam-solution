6790ea7c2f5178f499c71358ad7790bd9e841114
unknown <YY443@ccs.local>
2025-03-14 04:27:44 -0700

    Add git-basic.txt

--- question1.js
+++ question1.js
@@ -1,5 +1,5 @@
-/*ALDWIN SARMIENTO BSIT 3-B
-https://github.com/Aldwin4/midterm-exam-solution/tree/main
+'ALDWIN SARMIENTO BSIT 3-B'
+/*https://github.com/Aldwin4/midterm-exam-solution/tree/main
 
 GIT add .
 GIT pull
